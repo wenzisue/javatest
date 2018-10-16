e86921e74ea3a5c3c08250f985a467de77d91b99
xiaowen.wu <xiaowen.wu@rotech.com>
2018-10-16 15:52:50 -0400

    add the source file of Box

---
+++ src/main/java/com/xiaowen/javatest/Box.java
@@ -0,0 +1,28 @@
+package com.xiaowen.javatest;
+
+import java.math.BigInteger;
+
+public class Box <T> {
+	
+	private T t;          
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public <U extends Number> void inspect(U u){
+        System.out.println("T: " + t.getClass().getName());
+        System.out.println("U: " + u.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+        integerBox.set(new Integer(10));
+        integerBox.inspect(BigInteger.valueOf(1000000));
+    }
+
+}
