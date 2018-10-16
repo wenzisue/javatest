57b9808515d05c1cc7dba0922e58eb6bc9e28da5
xiaowen.wu <xiaowen.wu@rotech.com>
2018-10-16 15:08:05 -0400

    change the pom.xml and LocalClassExample

--- src/main/java/com/xiaowen/javatest/LocalClassExample.java
+++ src/main/java/com/xiaowen/javatest/LocalClassExample.java
@@ -28,19 +28,17 @@ public class LocalClassExample {
 			}
 
 			// Valid in JDK 8 and later:
-
-			// public void printOriginalNumbers() {
-			// System.out.println("Original numbers are " + phoneNumber1 +
-			// " and " + phoneNumber2);
-			// }
+			 public void printOriginalNumbers() {
+			 System.out.println("Original numbers are " + phoneNumber1 +
+			 " and " + phoneNumber2);
+			 }
 		}
 
 		PhoneNumber myNumber1 = new PhoneNumber(phoneNumber1);
 		PhoneNumber myNumber2 = new PhoneNumber(phoneNumber2);
 
 		// Valid in JDK 8 and later:
-
-		// myNumber1.printOriginalNumbers();
+		myNumber1.printOriginalNumbers();
 
 		if (myNumber1.getNumber() == null)
 			System.out.println("First number is invalid");
